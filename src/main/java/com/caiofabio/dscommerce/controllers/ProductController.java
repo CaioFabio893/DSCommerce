fbeb5d186523a2805705d217afaca3f69bb5aaee
Caio Fábio <caiofabio893@gmail.com>
2025-10-15 14:54:56 -0300

    put

--- src/main/java/com/caiofabio/dscommerce/controllers/ProductController.java
+++ src/main/java/com/caiofabio/dscommerce/controllers/ProductController.java
@@ -1,16 +1,17 @@
 package com.caiofabio.dscommerce.controllers;
 
 import com.caiofabio.dscommerce.dto.ProductDTO;
-import com.caiofabio.dscommerce.entities.Product;
-import com.caiofabio.dscommerce.repositories.ProductRepository;
+
 import com.caiofabio.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Optional;
+import java.net.URI;
+
 
 @RestController
 @RequestMapping(value = "/products")
@@ -19,8 +20,36 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    // busca de produtos por id
+    // ok retorna 200
     @GetMapping(value = "/{id}")
-    public ProductDTO findById( @PathVariable Long id) {
-       return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    // busca todos os prodtos paginadas
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    // inserir dados nome, descriçao, preço, imagem
+    // created retorn 201
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+
+    }
+
+    // atualizar pelo id 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/caiofabio/dscommerce/services/ProductService.java
+++ src/main/java/com/caiofabio/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.caiofabio.dscommerce.dto.ProductDTO;
 import com.caiofabio.dscommerce.entities.Product;
 import com.caiofabio.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,10 +18,52 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+
+    // buscar produto por id
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
+
+    // buscar todos os produtos listado
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+    // inserir produtos - nome, descriçao, preço, imagem
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+        //o metodo foi criado embaixo, boas praticas
+        copyDtoEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+
+    // atualizar pelo id
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+
+        Product entity = repository.getReferenceById(id);
+        //o metodo foi criado embaixo, boas praticas
+        copyDtoEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+
+    // metodo craido para cadrasto e atualizacao
+    private void copyDtoEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+    }
+
 
 }
