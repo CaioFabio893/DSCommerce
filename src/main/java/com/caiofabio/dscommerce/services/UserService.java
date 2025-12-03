e1521555812a5b945d2f8517d81c3fc72b99175f
Caio Fábio <caiofabio893@gmail.com>
2025-12-03 18:14:15 -0300

    achei o erro

--- src/main/java/com/caiofabio/dscommerce/services/UserService.java
+++ src/main/java/com/caiofabio/dscommerce/services/UserService.java
@@ -48,7 +48,7 @@ public class UserService implements UserDetailsService {
             Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
             String username = jwtPrincipal.getClaim("username");
 
-            return repository.findByEmail("username").get();
+            return repository.findByEmail(username).get();
         }
         catch (Exception e){
             throw new UsernameNotFoundException("User not found");
